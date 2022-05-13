99116c74ba686cc9cfe483e517bee23c2e24ce9e
hyewonJoo <run98@naver.com>
2022-05-14 02:11:28 +0900

    linkedlist build for another way

---
+++ Node2.java
@@ -0,0 +1,14 @@
+package list2;
+
+public class Node {
+	public String data;
+	public Node next; // 그 다음 노드
+
+	// 생성자,초기화
+	public Node(String item) {
+		data = item; // int
+		next = null; // node(class)
+	}
+
+
+}
